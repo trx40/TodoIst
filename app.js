6f7ea11951a860865fb630492b27e3c348fc48d0
trx40 <trm2468@gmail.com>
2022-09-26 03:11:26 +0530

    Fixed paths for css and ajax
    Use
    app.use(express.static('path/to/static/assets'))
    for CSS and other static files so express avoids routing on those paths

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static('public'));
 
 app.listen(3000, () => {
     console.log('APP IS LISTENING ON PORT 3000!')
--- public/ajax.js
+++ public/ajax.js
@@ -1,1 +1,2 @@
-alert('this works');
+
+alert("this works!");
